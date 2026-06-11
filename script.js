cfbac448eeb8dba00c9d1eaf4533317d4ebfd7d9
Positron-CY <pg0442147@gmail.com>
2026-06-11 13:31:49 +0530

    script.js

--- script.js
+++ script.js
@@ -1,109 +1,103 @@
-const video =
-document.getElementById("video");
+const video=document.getElementById("video");
+const glove=document.getElementById("glove");
+const beam=document.getElementById("beam");
+const scanner=document.getElementById("scanner");
+const power=document.getElementById("power");
 
 
-const glove =
-document.getElementById("glove");
-
-
-const beam =
-document.getElementById("beam");
-
-
-
-let sx=0;
-let sy=0;
-
+let sx=0, sy=0;
 let oldSize=0;
+let charge=0;
 
 
+const hands=new Hands({
 
-const hands =
-new Hands({
+locateFile:f=>
 
-locateFile:file =>
-
-`https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+`https://cdn.jsdelivr.net/npm/@mediapipe/hands/${f}`
 
 });
-
 
 
 hands.setOptions({
 
 maxNumHands:1,
-
 modelComplexity:1,
-
 minDetectionConfidence:.7,
-
 minTrackingConfidence:.7
 
 });
 
 
 
-hands.onResults(result=>{
+hands.onResults(r=>{
 
 
-if(result.multiHandLandmarks &&
-result.multiHandLandmarks.length){
+if(r.multiHandLandmarks &&
+r.multiHandLandmarks.length){
 
 
-let h=result.multiHandLandmarks[0];
+let h=r.multiHandLandmarks[0];
 
 
 let palm=h[9];
-
 let wrist=h[0];
 
 
-
 let x=(1-palm.x)*innerWidth;
-
 let y=palm.y*innerHeight;
 
 
-
-// smoothing
-
-sx += (x-sx)*0.25;
-
-sy += (y-sy)*0.25;
+sx+=(x-sx)*.25;
+sy+=(y-sy)*.25;
 
 
-
-let size =
+let size=
 Math.abs(wrist.y-h[12].y)*700;
 
 
 
 glove.style.display="block";
+scanner.style.display="block";
 
 
 glove.style.left=
-(sx-size/2)+"px";
-
+sx-size/2+"px";
 
 glove.style.top=
-(sy-size/1.8)+"px";
+sy-size/1.8+"px";
 
 
 glove.style.transform=
-
 `scale(${size/220})`;
 
 
 
+scanner.style.left=
+sx-125+"px";
 
-// blast detection
+scanner.style.top=
+sy-125+"px";
 
+
+
+charge++;
+
+if(charge>100)
+charge=100;
+
+
+power.innerHTML=
+charge+"%";
+
+
+
+// blast
 
 if(size-oldSize>15){
 
 
 beam.style.display="block";
-
 
 beam.style.left=sx+"px";
 
@@ -112,6 +106,7 @@ beam.style.top=sy+"px";
 beam.style.width=
 innerWidth+"px";
 
+createParticle();
 
 }
 
@@ -124,52 +119,61 @@ beam.style.display="none";
 
 oldSize=size;
 
-
 }
-
 
 else{
 
-
 glove.style.display="none";
-
+scanner.style.display="none";
 beam.style.display="none";
 
+charge=0;
+
+power.innerHTML="0%";
+
+}
+
+
+});
+
+
+
+function createParticle(){
+
+let p=document.createElement("div");
+
+p.className="particle";
+
+p.style.left=sx+"px";
+
+p.style.top=sy+"px";
+
+document.body.appendChild(p);
+
+
+setTimeout(()=>{
+
+p.remove();
+
+},1000);
 
 }
 
 
 
-});
 
-
-
-
-
-const camera =
-new Camera(video,{
-
+const camera=new Camera(video,{
 
 onFrame:async()=>{
 
-
-await hands.send({
-
-image:video
-
-});
-
+await hands.send({image:video});
 
 },
 
-
 width:1280,
-
 height:720
 
-
 });
-
 
 
 camera.start();
